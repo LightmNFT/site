55ba441b86f59381ef2d94b10b5f7d1690b1c423
Ayuilos <ekklovepeace@gmail.com>
2022-12-09 16:38:19 +0800

    feat: The first edition of Lightm official site.

---
+++ src/App.tsx
@@ -0,0 +1,296 @@
+import * as ZR from "zrender";
+import Zdog from "zdog";
+import { useCallback, useEffect, useState } from "react";
+import Canva from "./components/Canvas";
+import { sleep } from "./utils";
+
+const TRANSPARENT = "rgba(0,0,0,0)";
+const WHITE = "white";
+const BLACK = "black";
+
+const RMRK_THEME_COLOR = "#ca2a77";
+const EIP_2535_THEME_COLOR = "#53e6f8";
+
+function App() {
+  const [themeColor, setThemeColor] = useState(
+    (() => {
+      const query = window.matchMedia("(prefers-color-scheme: dark)");
+
+      if (query.matches) {
+        return WHITE;
+      } else {
+        return BLACK;
+      }
+    })()
+  );
+
+  const listenThemeChange = useCallback(() => {
+    const query = window.matchMedia("(prefers-color-scheme: dark)");
+
+    function handleColorSchemeChange(e: MediaQueryListEvent) {
+      if (e.matches) {
+        setThemeColor(WHITE);
+      } else {
+        setThemeColor(BLACK);
+      }
+    }
+
+    query.addEventListener("change", handleColorSchemeChange);
+  }, []);
+
+  const renderNestable = useCallback(
+    async (stage: ZR.ZRenderType) => {
+      if (stage) {
+        const stageWidth = stage.getWidth();
+        const stageHeight = stage.getHeight();
+        const radius = Math.min(stageWidth, stageHeight) / 12;
+        const sideLen = (radius * 224) / 128;
+
+        const NFTA = new ZR.Circle({
+          shape: {
+            cx: stageWidth / 3,
+            cy: stageHeight / 2,
+            r: radius,
+          },
+          style: { fill: TRANSPARENT },
+          silent: true,
+        });
+
+        const NFTB = new ZR.Rect({
+          shape: {
+            x: (stageWidth * 2) / 3 - sideLen / 2,
+            y: stageHeight / 2 - sideLen / 2,
+            width: sideLen,
+            height: sideLen,
+            r: 8,
+          },
+          style: { fill: TRANSPARENT },
+          silent: true,
+        });
+
+        stage.add(NFTA);
+        stage.add(NFTB);
+
+        NFTA.animate("style").when(500, { fill: themeColor }).start();
+        NFTA.animate("shape")
+          .delay(500)
+          .when(500, {
+            cx: stageWidth / 2,
+            cy: stageHeight / 3,
+          })
+          .start();
+
+        NFTB.animate("style")
+          .when(500, {
+            fill: themeColor,
+          })
+          .start();
+        NFTB.animate("shape")
+          .delay(500)
+          .when(500, {
+            x: stageWidth / 2 - sideLen / 2,
+            y: (stageHeight * 2) / 3 - sideLen / 2,
+          })
+          .done(drawLine)
+          .start();
+
+        function drawLine() {
+          const NFTARect = NFTA.getBoundingRect();
+          const NFTBRect = NFTB.getBoundingRect();
+          const startPoint = { x: NFTBRect.x + sideLen / 2, y: NFTBRect.y };
+          const finalPoint = {
+            x: NFTARect.x + radius,
+            y: NFTARect.y + radius * 2,
+          };
+
+          const line = new ZR.Line({
+            shape: {
+              x1: startPoint.x,
+              y1: startPoint.y,
+              x2: startPoint.x,
+              y2: startPoint.y,
+            },
+            style: { stroke: themeColor, fill: themeColor, lineWidth: 4 },
+            silent: true,
+          });
+
+          stage.add(line);
+
+          line
+            .animate("shape")
+            .when(800, {
+              x1: startPoint.x,
+              y1: startPoint.y,
+              x2: finalPoint.x,
+              y2: finalPoint.y,
+            })
+            .start();
+        }
+      }
+    },
+    [themeColor]
+  );
+
+  const renderMultiAsset = useCallback(
+    async (stage: ZR.ZRenderType) => {
+      if (stage) {
+        const stageWidth = stage.getWidth() / 2;
+        const stageHeight = stage.getHeight() / 2;
+        const radius = Math.min(stageWidth, stageHeight) / 12;
+        const sideLen = (radius * 224) / 128;
+
+        const NFTC = new ZR.Isogon({
+          shape: {
+            n: 6,
+            r: sideLen * 2,
+            x: stageWidth,
+            y: stageHeight,
+          },
+          style: {
+            fill: TRANSPARENT,
+          },
+          silent: true,
+        });
+
+        stage.add(NFTC);
+
+        NFTC.animate("style").when(500, { fill: themeColor }).start();
+        NFTC.animate("shape")
+          .delay(500)
+          .when(500, { n: 8 })
+          .when(1000, { n: 8 })
+          .when(1500, { n: 4 })
+          .when(2000, { n: 4 })
+          .when(2500, { n: 6 })
+          .start();
+      }
+    },
+    [themeColor]
+  );
+
+  const renderComposable = useCallback(async (illo: Zdog.Illustration) => {
+    const containerHeight = (illo.element.height as number) / devicePixelRatio;
+    const containerWidth = (illo.element.width as number) / devicePixelRatio;
+
+    const rainbowColors = [
+      "#9400d3",
+      "#4b0082",
+      "#0000ff",
+      "#00ff00",
+      "#ffff00",
+      "#ff7f00",
+      "#ff0000",
+    ];
+
+    const startY = -containerHeight - 20;
+    const endY = containerHeight / 6;
+    const diffrence = endY - startY;
+
+    rainbowColors.forEach(async (color, i) => {
+      await sleep(i * 800);
+
+      const semiRing = new Zdog.Ellipse({
+        addTo: illo,
+        diameter: containerWidth / 3 + i * 2 * 43,
+        quarters: 2,
+        stroke: 40,
+        color,
+        rotate: { z: -Zdog.TAU / 4, x: -Zdog.TAU / 120 },
+        translate: { y: startY },
+      });
+
+      let tick = 0;
+      const cycle = 90;
+
+      const animate = () => {
+        const progress = tick / cycle;
+        const tween = Zdog.easeInOut(progress, 3);
+
+        semiRing.translate.y = startY + diffrence * tween;
+
+        tick++;
+
+        if (semiRing.translate.y <= endY) {
+          requestAnimationFrame(animate);
+        }
+      };
+
+      animate();
+    });
+
+    illo.updateRenderGraph();
+  }, []);
+
+  useEffect(() => {
+    listenThemeChange();
+  }, []);
+
+  return (
+    <div className="p-4 dark:bg-black dark:text-white font-semibold flex flex-col items-center gap-4 w-full min-h-screen">
+      <div className="self-start flex flex-wrap items-end gap-4">
+        <div className="flex items-center">
+          <picture className="inline w-24">
+            <source
+              srcSet="LightmDark_logo.svg"
+              media="(prefers-color-scheme: dark)"
+            />
+            <img src="Lightm_logo.svg" />
+          </picture>
+          <h1 className="text-8xl">Lightm</h1>
+        </div>
+        <span className="text-sm">
+          Show respect to{" "}
+          <a
+            style={{ color: RMRK_THEME_COLOR }}
+            className="hover:underline hover:underline-offset-4"
+            href="https://rmrk.app"
+          >
+            RMRK
+          </a>{" "}
+          and{" "}
+          <a
+            style={{ color: EIP_2535_THEME_COLOR }}
+            className="hover:underline hover:underline-offset-4"
+            href="https://eips.ethereum.org/EIPS/eip-2535"
+          >
+            EIP-2535
+          </a>
+        </span>
+      </div>
+      <p className="self-start text-xl">
+        Next generation decentralized NFT platform.
+        <p>©{new Date().getFullYear()} LightM Labs.</p>
+      </p>
+      <div className="flex-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center items-center gap-4 w-full">
+        <Canva
+          id="nestable"
+          title="Nestable"
+          description="NFT can own NFT"
+          color={themeColor}
+          autoplay
+          render={renderNestable}
+        />
+        <Canva
+          id="multi-asset"
+          title="Multi Asset"
+          description="NFT can have multiple output"
+          color={themeColor}
+          autoplay
+          render={renderMultiAsset}
+        />
+        <Canva
+          is3D
+          className="colorful-box md:col-start-1 md:col-end-3 lg:col-auto"
+          textClassName="colorful-text"
+          id="composable"
+          title="Composable"
+          description="NFT can combine"
+          autoplay
+          render3D={renderComposable}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,143 @@
+import * as ZR from "zrender";
+import Zdog from "zdog";
+import { useCallback, useEffect, useRef } from "react";
+
+interface ICanvas {
+  id: string;
+  is3D?: boolean;
+  title: string;
+  description: string;
+  autoplay: boolean;
+  color?: string;
+  className?: string;
+  textClassName?: string;
+  render?: (stage: ZR.ZRenderType) => void;
+  render3D?: (illo: Zdog.Illustration) => void;
+}
+
+const RefreshIcon = ({ color }: { color?: string }) => (
+  <svg
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    width="48"
+    height="48"
+    fill={color || "auto"}
+  >
+    <path
+      d="M832 512a32 32 0 0 0-32 32c0 158.784-129.216 288-288 288s-288-129.216-288-288 129.216-288 288-288c66.208 0 129.536 22.752 180.608 64H608a32 32 0 0 0 0 64h160a32 32 0 0 0 32-32V192a32 32 0 0 0-64 0v80.96A350.464 350.464 0 0 0 512 192C317.92 192 160 349.92 160 544s157.92 352 352 352 352-157.92 352-352a32 32 0 0 0-32-32"
+      p-id="1361"
+    ></path>
+  </svg>
+);
+
+export default function Canvas({
+  id,
+  is3D,
+  title,
+  description,
+  autoplay,
+  color,
+  className,
+  textClassName,
+  render,
+  render3D,
+}: ICanvas) {
+  const domRef = useRef<HTMLDivElement>(null);
+  const dom3DRef = useRef<HTMLCanvasElement>();
+
+  const stageRef = useRef<ZR.ZRenderType>();
+  const illoRef = useRef<Zdog.Illustration>();
+
+  id = `example-display-container-${id}`;
+
+  const init = useCallback(() => {
+    const stage = ZR.init(domRef.current);
+
+    stageRef.current = stage;
+  }, []);
+
+  const init3D = useCallback(() => {
+    const container = domRef.current!;
+    const canvas = document.createElement("canvas");
+    canvas.width = container.clientWidth * 2 * devicePixelRatio;
+    canvas.height = container.clientHeight * 2 * devicePixelRatio;
+
+    if (dom3DRef.current) {
+      dom3DRef.current.remove();
+    }
+
+    container.appendChild(canvas);
+
+    const illo = new Zdog.Illustration({
+      element: canvas,
+      dragRotate: true,
+    });
+
+    canvas.style.width = `100%`;
+    canvas.style.height = `100%`;
+
+    illoRef.current = illo;
+    dom3DRef.current = canvas;
+
+    function animate() {
+      illo.updateRenderGraph();
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    return () => {};
+  }, []);
+
+  const reRender = useCallback(() => {
+    stageRef.current?.clear();
+    render && render(stageRef.current!);
+  }, [render]);
+
+  const reRender3D = useCallback(() => {
+    init3D();
+    render3D && render3D(illoRef.current!);
+  }, [init3D]);
+
+  useEffect(() => {
+    (is3D ? init3D : init)();
+
+    if (autoplay) {
+      (is3D ? reRender3D : reRender)();
+    }
+  }, []);
+
+  useEffect(() => {
+    (is3D ? reRender3D : reRender)();
+  }, [reRender, reRender3D]);
+
+  return (
+    <div
+      className={`box-border min-w-full aspect-square relative border-black border-4 rounded-2xl dark:border-white${
+        className ? ` ${className}` : ""
+      }`}
+      style={{
+        maxWidth: "min(640px,38%)",
+        maxHeight: "100%",
+      }}
+    >
+      <div className="w-full h-full relative" ref={domRef} id={id}></div>
+      <div
+        className={`absolute top-2 right-2 flex items-center${
+          textClassName ? ` ${textClassName}` : ""
+        }`}
+      >
+        <div className="flex flex-col items-end cursor-default">
+          <span className="text-2xl">{title}</span>
+          <span className="text-md">{description}</span>
+        </div>
+        <span
+          className="cursor-pointer hover:scale-110 transition-transform"
+          onClick={is3D ? reRender3D : reRender}
+        >
+          <RefreshIcon color={color} />
+        </span>
+      </div>
+    </div>
+  );
+}
