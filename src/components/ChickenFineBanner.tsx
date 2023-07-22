dc3398fc9c865c76bff9c92fa5020f6c8fe927eb
Ayuilos <ekklovepeace@gmail.com>
2023-07-22 10:40:28 +0800

    feat: add close btn to banner

--- src/components/ChickenFineBanner.tsx
+++ src/components/ChickenFineBanner.tsx
@@ -1,8 +1,11 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const chickenFineLink = "https://chicken-fine-site.vercel.app/";
 
 export default function ChickenFineBanner() {
+  const [close, setClose] = useState(false);
+
   return (
     <motion.div
       initial={{ scale: 0.8 }}
@@ -12,7 +15,9 @@ export default function ChickenFineBanner() {
         type: "spring",
         damping: 25,
       }}
-      className="fixed flex justify-center items-center rounded-xl overflow-hidden right-8 bottom-8 max-lg:right-auto z-50 w-[42%] max-2xl:w-[54%] max-lg:w-[94%]"
+      className={`fixed flex justify-center items-center rounded-xl overflow-hidden right-8 bottom-8 max-lg:right-auto z-50 w-[42%] max-2xl:w-[54%] max-lg:w-[94%]${
+        close ? " hidden" : ""
+      }`}
     >
       <div className="absolute z-20 p-2">
         <div className="flex flex-col items-center gap-4 pt-16">
@@ -35,6 +40,12 @@ export default function ChickenFineBanner() {
           className="w-24 h-24 max-md:w-12 max-md:h-12"
         />
         <h2 className="text-3xl">Chicken Fine</h2>
+      </div>
+      <div
+        className="absolute z-30 top-4 right-4 text-3xl cursor-pointer"
+        onClick={() => setClose(true)}
+      >
+        x
       </div>
       <div className="absolute left-0 right-0 top-0 bottom-0 bg-white/30 dark:bg-black/30 z-10 backdrop-blur-sm"></div>
       <img src="/bg.png" alt="chicken_fine_bg" className="w-full" />
